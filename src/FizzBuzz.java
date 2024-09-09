72b3e39c0dcf17aa5ef15b8c96a0d1ad327ab28c
Jerry Zeng <jerryyujiezeng@Jerry-Zengs-MacBook-Air.local>
2024-09-09 17:51:14 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,30 +5,56 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
-
+        for (int i = 1; i < 100; i++) {doFizzBuzz(i);}
+        int ii = 1;
+        while (ii <= 100) {
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            boolean divisibleBy3_while = ii % 3 == 0;
+            boolean divisibleBy5_while = ii % 5 == 0;
 
             // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+            if (divisibleBy3_while && divisibleBy5_while) {
 
-                System.out.println("Fizz Buzz");
+                System.out.println("Fizz Buzz while");
 
-            } else if (divisibleBy3) {
+            } else if (divisibleBy3_while) {
 
-                System.out.println("Fizz");
+                System.out.println("Fizz while");
 
-            } else if (divisibleBy5) {
+            } else if (divisibleBy5_while) {
 
-                System.out.println("Buzz");
+                System.out.println("Buzz while");
 
             } else {
 
-                System.out.println(i);
-
+                System.out.println(ii);
             }
+            ii += 1;
+        }
+    }
+
+    private static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
         }
     }
 }
